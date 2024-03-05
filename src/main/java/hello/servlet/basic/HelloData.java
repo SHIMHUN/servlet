1aa43769c117067d70069dafbe6b7dbd8101b7ab
SHIMHUN <shimhun88@naver.com>
2024-03-05 22:34:08 +0900

    Initial commit

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+    private String username;
+    private int age;
+
+    //롬복 라이브러리 적용 후 필요 x
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+}
